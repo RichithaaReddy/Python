9b4f8a01cac668979f478b3a21829bfcb61a7c80
richitha <richithareddie@gmail.com>
2023-02-03 22:41:20 +0530

    added

---
+++ add.py
@@ -0,0 +1,5 @@
+a = input("enter value of a: ")
+b = input("enter value of b: ")
+sum = 0 #just to avoid garbage value (initialising)
+sum = int(a) + int(b)  #type coversion as it takes the value as string
+print("sum of a and b",sum)
